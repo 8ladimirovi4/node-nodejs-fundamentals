67004c82b855c131e2ad853ee2a6e715a498a08f
Vladimir_Leonov <2227331.spb@gmail.com>
2026-03-07 14:46:21 +0300

    feat: implement dynamic.js

--- src/modules/dynamic.js
+++ src/modules/dynamic.js
@@ -1,9 +1,33 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const dynamic = async () => {
   // Write your code here
   // Accept plugin name as CLI argument
   // Dynamically import plugin from plugins/ directory
   // Call run() function and print result
   // Handle missing plugin case
+  await runModule();
 };
 
 await dynamic();
+
+async function runModule() {
+  const plugin = process.argv[2];
+  if (!plugin) {
+    console.error('Plugin not found');
+    process.exit(1);
+  }
+
+  try {
+    const url = import.meta.url;
+    const __dirname = path.dirname(fileURLToPath(url));
+    const modulePath = path.join(__dirname, 'plugins', `${plugin}.js`);
+    const module = await import(modulePath);
+
+    console.log(module.run());
+  } catch (error) {
+    console.error('Plugin not found');
+    process.exit(1);
+  }
+}
