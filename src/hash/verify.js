4b9901cf9da613b2aee7e69ca775317f03a2af79
Vladimir_Leonov <2227331.spb@gmail.com>
2026-03-07 16:43:30 +0300

    feat: implement verify method

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,71 @@
+import fs from 'fs/promises';
+import { createReadStream } from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+
 const verify = async () => {
   // Write your code here
   // Read checksums.json
   // Calculate SHA256 hash using Streams API
   // Print result: filename — OK/FAIL
+
+  //create file_*.txt in the root of workspace dir
+  //create checksums.json in the root dir
+
+  const checksumsFilePath = path.resolve('checksums.json');
+  const txtFilesDir = 'workspace';
+
+  await isWorkspaceExist(checksumsFilePath);
+
+  const json = await readFileData(checksumsFilePath);
+
+  for (const [file, expectedHash] of Object.entries(json)) {
+    const filePath = path.join(txtFilesDir, file);
+    try {
+      const actualHash = await calculateHash(filePath);
+
+      if (actualHash === expectedHash) {
+        console.log(`${file} — OK`);
+      } else {
+        console.log(`${file} — FAIL`);
+      }
+    } catch {
+      console.log(`${file} — FAIL`);
+    }
+  }
 };
 
 await verify();
+
+function calculateHash(file) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha256');
+    const stream = createReadStream(file);
+
+    stream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+      resolve(hash.digest('hex'));
+    });
+
+    stream.on('error', reject);
+  });
+}
+
+async function readFileData(filePath) {
+  const fileData = await fs.readFile(filePath, {
+    encoding: 'utf8',
+  });
+  return JSON.parse(fileData);
+}
+
+async function isWorkspaceExist(filePath) {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch {
+    throw new Error('FS operation failed');
+  }
+}
