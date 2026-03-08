6a551f1adf7d6f1fbcc03f159c1f1a020d15bc63
Vladimir_Leonov <2227331.spb@gmail.com>
2026-03-08 23:22:03 +0300

    feat: implement execCommand method

--- src/cp/execCommand.js
+++ src/cp/execCommand.js
@@ -1,10 +1,29 @@
+import { spawn } from 'child_process';
+
 const execCommand = () => {
-  // Write your code here
+    // Write your code here
   // Take command from CLI argument
   // Spawn child process
   // Pipe child stdout/stderr to parent stdout/stderr
   // Pass environment variables
   // Exit with same code as child
+
+  const command = process.argv[2];
+  if (!command) {
+    process.exit(1);
+  }
+
+  const child = spawn(command, {
+    shell: true,
+    env: process.env,
+  });
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on('close', (code) => {
+    process.exit(code ?? 1);
+  });
 };
 
 execCommand();
