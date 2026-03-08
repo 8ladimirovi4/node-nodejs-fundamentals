ffef07811c1daf50d60d858ddc9835f3ce5b5aaf
Vladimir_Leonov <2227331.spb@gmail.com>
2026-03-08 17:33:02 +0300

    fix: add description to split method

--- src/streams/split.js
+++ src/streams/split.js
@@ -2,6 +2,11 @@ import { createReadStream, createWriteStream, readdirSync, unlinkSync } from "fs
 import path from "path";
 
 const split = async () => {
+    // Write your code here
+  // Read source.txt using Readable Stream
+  // Split into chunk_1.txt, chunk_2.txt, etc.
+  // Each chunk max N lines (--lines CLI argument, default: 10)
+  
   const sourceFile = path.resolve("source.txt");
   const cwd = process.cwd();
   readdirSync(cwd)
